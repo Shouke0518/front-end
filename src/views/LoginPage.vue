1043508ceedaa18ff386a00a853af7183ecae494
O Ko TuTuUTa <shit141414@gmail.com>
2024-12-17 22:27:52 +0800

    render adjust

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -107,10 +107,10 @@ export default {
     },
 
     loginByGoogle() {
-      window.location.href = process.env.BACKEND_URL || 'http://localhost:3000';
+      window.location.href = "https://back-end-cwng.onrender" || 'http://localhost:3000';
     },
     loginByGithub() {
-      window.location.href = process.env.BACKEND_URL || 'http://localhost:3000';
+      window.location.href = "https://back-end-cwng.onrender" || 'http://localhost:3000';
     },
     showPassword() {
       this.passwordVisible = !this.passwordVisible;
